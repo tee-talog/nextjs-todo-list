2f3fc79957ecc1c4bc134987cf4656cc7bf3c193
talog <tee.talog@gmail.com>
2022-05-03 23:08:54 +0900

    feat: 削除処理実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import clsx from 'clsx'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { ChangeEvent, FormEventHandler, useEffect, useState } from 'react'
+import {
+  ChangeEvent,
+  FormEventHandler,
+  MouseEventHandler,
+  useEffect,
+  useState,
+} from 'react'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import List from '../components/List'
@@ -10,8 +16,9 @@ import PageTitle from '../components/PageTitle'
 import SectionTitle from '../components/SectionTitle'
 
 // types
+type ItemId = string
 type Item = {
-  id: string
+  id: ItemId
   title: string
 }
 
@@ -30,6 +37,10 @@ const Container: NextPage = () => {
     setItems((items) => [...items, item])
   }
 
+  const removeItem = (id: ItemId) => {
+    setItems((items) => items.filter((item) => item.id !== id))
+  }
+
   // debug
   useEffect(() => {
     addItem({ title: 'タスクその 1' })
@@ -37,16 +48,17 @@ const Container: NextPage = () => {
     addItem({ title: 'タスクその 3' })
   }, [])
 
-  return <Presenter items={items} addItem={addItem} />
+  return <Presenter items={items} addItem={addItem} removeItem={removeItem} />
 }
 
 // presentation
 type PresenterProps = {
   items: Item[]
   addItem: (payload: AddItemPayload) => void
+  removeItem: (id: ItemId) => void
 }
 
-const Presenter = ({ items, addItem }: PresenterProps) => {
+const Presenter = ({ items, addItem, removeItem }: PresenterProps) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
@@ -72,7 +84,7 @@ const Presenter = ({ items, addItem }: PresenterProps) => {
 
         <section className="w-full">
           <SectionTitle className="sr-only">Items</SectionTitle>
-          <ItemList items={items} />
+          <ItemList items={items} removeItem={removeItem} />
         </section>
       </main>
     </div>
@@ -115,19 +127,28 @@ const AddItemForm = ({ addItem }: AddItemFormProps) => {
 
 type ItemListProps = {
   items: Item[]
+  removeItem: (id: ItemId) => void
 }
 
-const ItemList = ({ items }: ItemListProps) => (
-  <List>
-    {items.map((item) => (
-      <ListItem key={item.id}>
-        <div className="flex items-center justify-between gap-x-2">
-          <p className="text-left">{item.title}</p>
-          <Button type="button">Remove</Button>
-        </div>
-      </ListItem>
-    ))}
-  </List>
-)
+const ItemList = ({ items, removeItem }: ItemListProps) => {
+  const handleClick = (id: ItemId) => {
+    removeItem(id)
+  }
+
+  return (
+    <List>
+      {items.map((item) => (
+        <ListItem key={item.id}>
+          <div className="flex items-center justify-between gap-x-2">
+            <p className="text-left">{item.title}</p>
+            <Button type="button" onClick={(_) => handleClick(item.id)}>
+              Remove
+            </Button>
+          </div>
+        </ListItem>
+      ))}
+    </List>
+  )
+}
 
 export default Container
