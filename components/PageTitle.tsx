b1b4d4bda932d48953a13f6185fc2ec543de6531
talog <tee.talog@gmail.com>
2022-05-03 17:57:34 +0900

    feat: ページタイトルコンポーネントにスタイルを付与

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,8 +1,16 @@
-import { ReactNode } from 'react'
+import clsx from 'clsx'
+import { ComponentProps } from 'react'
 
-type PageTitleProps = {
-  children: ReactNode
-}
-const PageTitle = ({ children }: PageTitleProps) => <h1>{children}</h1>
+type PageTitleProps = ComponentProps<'h1'>
 
-export default PageTitle
+const Container = ({ children, ...props }: PageTitleProps) => (
+  <Presenter {...props}>{children}</Presenter>
+)
+
+const Presenter = ({ children, className, ...props }: PageTitleProps) => (
+  <h1 className={clsx(className, 'text-4xl')} {...props}>
+    {children}
+  </h1>
+)
+
+export default Container
