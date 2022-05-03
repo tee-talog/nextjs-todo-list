2f8c0635f78f9c6c6ec2f3c3834bdb41c17569f4
talog <tee.talog@gmail.com>
2022-05-03 19:19:16 +0900

    feat: ページ全体のレイアウトを調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Button from '../components/Button'
@@ -9,28 +10,40 @@ import SectionTitle from '../components/SectionTitle'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
         <title>TODO List</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <PageTitle>TODO List</PageTitle>
+      <header className="flex h-16 w-full items-center bg-green-200 drop-shadow">
+        <PageTitle className="px-2">TODO List</PageTitle>
+      </header>
 
-        <section>
-          <SectionTitle>add item</SectionTitle>
-          <form>
-            <Input type="text" placeholder="例：TODO リストを見る" />
+      <main
+        className={clsx(
+          'w-full max-w-screen-md px-4 py-8',
+          'flex flex-1 flex-col items-center gap-y-8',
+          'text-center'
+        )}
+      >
+        <section className="flex w-full flex-col items-center">
+          <SectionTitle className="sr-only">Add Item</SectionTitle>
+          <form className="flex w-full flex-nowrap">
+            <Input
+              type="text"
+              placeholder="例：TODO リストを確認する"
+              className="mr-4 flex-1"
+            />
             <Button type="submit">Add</Button>
           </form>
         </section>
 
-        <section>
-          <SectionTitle>items</SectionTitle>
+        <section className="w-full">
+          <SectionTitle className="sr-only">Items</SectionTitle>
           <List>
             <ListItem>
-              <div className="flex gap-x-2">
+              <div className="flex justify-between gap-x-2">
                 <h3>item</h3>
                 <div>
                   <button type="button">Remove</button>
